54f3633aa831f028343f330f3e909dc3e2ae96ed
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-10-12 13:20:19 +0530

    removed space

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -4,7 +4,7 @@ export const theme = {
     ...DefaultTheme,
     colors: {
         ...DefaultTheme.colors,
-        // primary: 'tomato', 
+        // primary: 'tomato',
         // secondary: 'orange',
     },
 };
