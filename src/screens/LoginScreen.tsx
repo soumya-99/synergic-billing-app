0f2f631a6aeb40867b5ccfda67a129213d56d10b
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-11-03 18:55:12 +0530

    design added loginscreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native"
 import { withTheme, Divider } from "react-native-paper"
 import { usePaperColorScheme } from "../theme/theme"
 import InputPaper from "../components/InputPaper"
@@ -13,23 +13,28 @@ function LoginScreen() {
   const [loginText, setLoginText] = useState("")
   const [passwordText, setPasswordText] = useState("")
   return (
-    <>
-    <View
-      style={[
-        styles.loginWrapper,
-        { backgroundColor: theme.colors.background },
-      ]}>
+    <ScrollView style={[
+      styles.loginWrapper,
+      { backgroundColor: theme.colors.background },
+    ]}>
+      {/* <View> */}
+
       <View
         style={{ margin: 20, justifyContent: "center", alignItems: "center" }}>
         <Text style={[styles.loginHeader, { color: theme.colors.primary }]}>
           Billing App
         </Text>
       </View>
-      <View style={{position: "absolute", width: 90, height: 150, backgroundColor: "red", borderRadius: 10, transform: [{ rotate: '45deg' }], top: 25, right: 260, opacity: 0.5}}></View>
-      
+      <View style={{position: "absolute", width: 90, height: 150, backgroundColor: "orange", borderRadius: 10, transform: [{ rotate: '45deg' }], top: 20, right: 260, opacity: 0.5}}></View>
+      <View style={{position: "absolute", width: 90, height: 150, backgroundColor: "dodgerblue", borderRadius: 30, transform: [{ rotate: '-65deg' }], top: 300, left: 260, opacity: 0.5}}></View>
+
       {/* <View style={{ padding: 5 }}>
         <Divider />
       </View> */}
+
+      <View style={{ flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Image source={require('../resources/images/billingapplogo.jpg')} style={{ height: 300, width: 300, borderRadius: 30 }} />
+      </View>
       <View style={styles.textFields}>
         <InputPaper
           value={loginText}
@@ -48,12 +53,12 @@ function LoginScreen() {
         <ButtonPaper mode="contained" onPress={login}>
           Login
         </ButtonPaper>
+        <View style={{ margin: 2, padding: 2 }}>
+          <Text style={{ fontSize: 10 }}>Powered by, Synergic Softek Solutions Pvt. Ltd.</Text>
+        </View>
       </View>
-      <View style={{margin: 5, padding: 5}}>
-        <Text style={{fontSize: 10}}>Powered by: Synergic Softek Solutions Pvt. Ltd.</Text>
-      </View>
-    </View>
-    </>
+      {/* </View> */}
+    </ScrollView>
   )
 }
 
@@ -66,6 +71,7 @@ const styles = StyleSheet.create({
 
   loginHeader: {
     fontSize: 40,
+    fontWeight: "500"
   },
 
   textFields: {
