ab71be2fc2091b58d2d3dbef7b1006cc24b47d7f
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-02-02 17:19:28 +0530

    Commas from models removed

--- src/models/api_types.ts
+++ src/models/api_types.ts
@@ -19,20 +19,20 @@ export type CreatePinData = {
 }
 
 export type LoginData = {
-    suc: 0 | 1,
+    suc: 0 | 1
     msg: {
-        comp_id: number,
-        company_name: string,
-        br_id: number,
-        branch_name: string,
-        user_id: string,
-        user_name: string,
-        phone_no: number,
-        email_id: string,
-        device_id: string,
-        password: string,
-        created_by: string,
-        created_dt: string,
+        comp_id: number
+        company_name: string
+        br_id: number
+        branch_name: string
+        user_id: string
+        user_name: string
+        phone_no: number
+        email_id: string
+        device_id: string
+        password: string
+        created_by: string
+        created_dt: string
         active_flag: "Y" | "N"
     } | ""
 }
