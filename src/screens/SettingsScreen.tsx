27f0666aae2b1d1514f607aa9e8ff3cd40131a07
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-02-16 11:59:12 +0530

    Logout alert added

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -11,12 +11,23 @@ import ReportButton from "../components/ReportButton"
 import { usePaperColorScheme } from "../theme/theme"
 import { CommonActions, useNavigation } from "@react-navigation/native"
 import navigationRoutes from "../routes/navigationRoutes"
+import { Button, Dialog, Portal, Text } from "react-native-paper"
 
 function SettingsScreen() {
   const navigation = useNavigation()
   const theme = usePaperColorScheme()
 
   const { handleLogout } = useContext(AppStore)
+
+  const [visible, setVisible] = useState(() => false)
+
+  const showDialog = () => setVisible(true)
+  const hideDialog = () => setVisible(false)
+
+  const loggingOut = () => {
+    handleLogout()
+    hideDialog()
+  }
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]}>
@@ -31,9 +42,22 @@ function SettingsScreen() {
           </HeaderImage>
         </View>
 
+        <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog.Title>Logging Out</Dialog.Title>
+            <Dialog.Content>
+              <Text variant="bodyMedium">Are you sure you want to log out?</Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={hideDialog}>Cancel</Button>
+              <Button onPress={loggingOut}>Yes</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+
         <View style={{ padding: 20 }}>
-          <ButtonPaper icon="logout" mode="text" onPress={handleLogout}>
-            Log Out
+          <ButtonPaper icon="logout" mode="text" onPress={showDialog}>
+            LOG OUT
           </ButtonPaper>
         </View>
 
