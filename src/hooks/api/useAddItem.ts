e1616a8a9a927bca7c75e3ff6f138f5714e6ca27
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-02-23 15:45:01 +0530

    Unnecessary comments removed

--- src/hooks/api/useAddItem.ts
+++ src/hooks/api/useAddItem.ts
@@ -2,20 +2,6 @@ import axios from "axios"
 import { ADDRESSES } from "../../config/api_list"
 import { AddItemCredentials, AddedItemResponse } from "../../models/api_types"
 
-// export default function useAddItem() {
-//     const sendAddedItem = async (addedItem: AddItemCredentials) => {
-//         return new Promise<AddedItemResponse>((resolve, reject) => {
-//             axios.post(`${ADDRESSES.ADD_ITEM}`, addedItem).then(res => {
-//                 resolve(res.data)
-//             }).catch(err => {
-//                 reject(err)
-//             })
-//         })
-//     }
-//     return { sendAddedItem }
-// }
-
-
 export default function useAddItem() {
     const sendAddedItem = async (addedItem: AddItemCredentials) => {
         return new Promise<AddedItemResponse>((resolve, reject) => {
