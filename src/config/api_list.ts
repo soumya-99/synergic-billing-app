4e7e8130849bdfa5e9e819e81e97eeee2e99132a
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-03-27 11:46:59 +0530

    Small typo in VERIFY_ACTIVE api

--- src/config/api_list.ts
+++ src/config/api_list.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "./config";
 
 export const ADDRESSES = {
     REGISTER: `${BASE_URL}/api/verify_phone`,
-    VERYFY_ACTIVE: `${BASE_URL}/api/verify_active`,
+    VERIFY_ACTIVE: `${BASE_URL}/api/verify_active`,
     OTP: `${BASE_URL}/api/otp`,
     CREATE_PIN: `${BASE_URL}/api/create_pin`,
     LOGIN: `${BASE_URL}/api/login`,
--- src/hooks/api/useVerifyActive.ts
+++ src/hooks/api/useVerifyActive.ts
@@ -7,7 +7,7 @@ import { BasicResponse } from "../../models/api_types"
 export default function useVerifyActive() {
     const verifyActive = async (phoneNumber: string) => {
         return new Promise<BasicResponse>((resolve, reject) => {
-            axios.post(`${ADDRESSES.VERYFY_ACTIVE}/${phoneNumber}`).then(res => {
+            axios.post(`${ADDRESSES.VERIFY_ACTIVE}/${phoneNumber}`).then(res => {
                 resolve(res.data)
             }).catch(err => {
                 reject(err)
